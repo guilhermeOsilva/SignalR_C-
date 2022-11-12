4ae5f4f2a6bc1104318ec5f56cc3d5e1639c0a2a
guilhermeOsilva <guilherme.osilva@bandtec.com.br>
2022-11-11 23:12:21 -0300

    criando a hub pro signalR

---
+++ SignalR/WebApplication1/Hubs/PollHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace SignalR.Hubs
+{
+    public class PollHub: Hub
+    {
+        public async Task SendMessage(string user, string message, string myChannelId, string myChannelVal)
+        {
+            await Clients.All.SendAsync("ReceiveMessae", user, message, myChannelVal).ConfigureAwait(false);
+        }
+    }
+}
